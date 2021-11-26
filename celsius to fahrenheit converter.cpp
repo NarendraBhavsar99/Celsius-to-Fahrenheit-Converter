07aa43725a867b213d462bb40ac7312b05923c78
NarendraBhavsar99 <95095281+NarendraBhavsar99@users.noreply.github.com>
2021-11-26 23:08:16 +0530

    Add files via upload

---
+++ celsius to fahrenheit converter.cpp
@@ -0,0 +1,12 @@
+//celsius to fahrenheit converter
+
+
+#include<stdio.h>
+
+int main(){
+	int celcius,a;
+	celcius= (40*9/5)+32;
+	
+	printf("print the value of celcius %d", celcius);
+	return 0;
+}
